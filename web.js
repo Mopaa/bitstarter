3f66b572e6b0b2b60e7ef9426f332ad900d60762
Mopaa <arosenberg77@gmail.com>
2013-07-11 03:56:05 +0000

    Changed web.js to read index.html

--- web.js
+++ web.js
@@ -1,10 +1,12 @@
 var express = require('express');
 var app = express.createServer(express.logger());
 var fs = require('fs');
+buf = new Buffer(256);
 
 app.get('/', function(request, response) {
   //response.send('Hello World2!');
-   fs.readFileSync('index.html');
+
+   fs.readFileSync(buf.toString('index.html'));
 });
 
 var port = process.env.PORT || 5000;
